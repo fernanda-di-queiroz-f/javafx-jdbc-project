99ec7986a4c233a577c44a521d1f6d11bf6373ee
Fernanda <fernanda1queiroz44@gmail.com>
2020-08-11 15:04:37 -0300

    Saving seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -134,9 +136,29 @@ public class SellerFormController implements Initializable {
 		}
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
+
+		obj.setDepartment(cbDepartment.getValue());
+
 		return obj;
 	}
 
@@ -191,9 +213,13 @@ public class SellerFormController implements Initializable {
 	private void setErrorMessages(Map<String, String> errors) {
 		Set<String> fields = errors.keySet();
 
-		if (fields.contains("name")) {
-			lbErrorName.setText(errors.get("name"));
-		}
+		lbErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+
+		lbErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+
+		lbErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
+
+		lbErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
 	}
 
 	private void initializeComboBoxDepartment() {
